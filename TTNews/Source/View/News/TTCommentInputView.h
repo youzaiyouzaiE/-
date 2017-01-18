9ee952db5bb4a0375c807a9684fad5b7d6f9e13d
jiahui <goodzhoujiahui@foxmail.com>
2017-01-18 14:04:10 +0800

    替换LaunchImage

---
+++ TTNews/Source/View/News/TTCommentInputView.h
@@ -0,0 +1,13 @@
+//
+//  TTCommentInputView.h
+//  TTNews
+//
+//  Created by jiahui on 2017/1/18.
+//  Copyright © 2017年 TTNews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTCommentInputView : UIView
+
+@end
---
+++ TTNews/Source/View/News/TTCommentInputView.m
@@ -0,0 +1,21 @@
+//
+//  TTCommentInputView.m
+//  TTNews
+//
+//  Created by jiahui on 2017/1/18.
+//  Copyright © 2017年 TTNews. All rights reserved.
+//
+
+#import "TTCommentInputView.h"
+
+@implementation TTCommentInputView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
